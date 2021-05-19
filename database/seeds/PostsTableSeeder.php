31a00833c3d572ce47bedaa1a22695d327bd2a9e
noor4apps <noor4apps@gmail.com>
2021-05-20 02:48:53 +0300

    #8

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -5,6 +5,8 @@ use App\Models\Post;
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class PostsTableSeeder extends Seeder
 {
@@ -66,15 +68,44 @@ class PostsTableSeeder extends Seeder
          * @throws \InvalidArgumentException
          */
 
+        /* public static function random($array, $number = null)
+         * Get one or a specified number of random values from an array.
+         */
+        $posts = [];
+        $days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
+        $months  = ['01', '02', '03', '04', '05', '06'];
+
         for($i = 0; $i < 1000; $i++) {
-            Post::create([
-                'title' => $faker->sentence(mt_rand(3, 6), true),
+            $post_date = "2021-" . Arr::random($months) . "-" . Arr::random($days) . " 01:01:01"; // "2021-05-10 01:01:01"
+            $post_title = $faker->sentence(mt_rand(3, 6), true);
+            $posts[] = [
+                'title' => $post_title,
+                'slug' => Str::slug($post_title),
                 'description' => $faker->paragraph,
                 'status' => rand(0, 1),
                 'comment_able' => rand(0, 1),
                 'user_id' => $users->random(),
-                'category_id' => $categories->random()
-            ]);
+                'category_id' => $categories->random(),
+                'created_at' => $post_date,
+                'updated_at' => $post_date,
+            ];
+        }
+
+        /* function array_chunk(array $input, $size, $preserve_keys = null) { }
+         * Split an array into chunks
+         * @link https://php.net/manual/en/function.array-chunk.php
+         * @param array $input
+         * The array to work on
+         * @param int $size
+         * The size of each chunk
+         * @param bool $preserve_keys [optional]
+         * When set to true keys will be preserved. Default is false which will reindex the chunk numerically
+         * @return array a multidimensional numerically indexed array, starting with zero,
+         * with each dimension containing size elements.
+         */
+        $chunks = array_chunk($posts, 100);
+        foreach ($chunks as $chunk) {
+            Post::insert($chunk);
         }
     }
 }
