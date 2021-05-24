a5cefffccd2fa60ba5f94bb1c90f3f3b40193c74
noor4apps <noor4apps@gmail.com>
2021-05-24 23:02:37 +0300

    #27

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
@@ -70,8 +73,40 @@ class ViewServiceProvider extends ServiceProvider
                 }
                 $recent_posts = Cache::get('recent_posts');
 
+                if(!Cache::has('recent_comments')) {
+                    $recent_comments = Comment::whereStatus(1)->orderBy('id', 'desc')->limit(5)->get();
+
+                    Cache::remember('recent_comments', 3600, function () use ($recent_comments) {
+                        return $recent_comments;
+                    });
+                }
+                $recent_comments = Cache::get('recent_comments');
+
+                if(!Cache::has('global_categories')) {
+                    $global_categories = Category::whereStatus(1)->orderBy('id', 'desc')->get();
+
+                    Cache::remember('global_categories', 3600, function () use ($global_categories) {
+                        return $global_categories;
+                    });
+                }
+                $global_categories = Cache::get('global_categories');
+
+                if(!Cache::has('global_archives')) {
+                    $global_archives = Post::whereStatus(1)->orderBy('created_at', 'desc')
+                        ->select(DB::raw("Year(created_at) as year"), DB::raw("Month(created_at) as month"))
+                        ->Pluck('year', 'month')->toArray();
+
+                    Cache::remember('global_archives', 3600, function () use ($global_archives) {
+                        return $global_archives;
+                    });
+                }
+                $global_archives = Cache::get('global_archives');
+
                 $view->with([
                     'recent_posts' => $recent_posts,
+                    'recent_comments' => $recent_comments,
+                    'global_categories' => $global_categories,
+                    'global_archives' => $global_archives,
                 ]);
             });
 
--- resources/views/partial/frontend/sidebar.blade.php
+++ resources/views/partial/frontend/sidebar.blade.php
@@ -42,61 +42,19 @@
     <aside class="widget comment_widget">
         <h3 class="widget-title">Comments</h3>
         <ul>
+            @foreach($recent_comments as $recent_comment)
             <li>
                 <div class="post-wrapper">
                     <div class="thumb">
-                        <img src="{{ asset('frontend/images/blog/comment/1.jpeg') }}" alt="Comment images">
+                        <img src="{{ get_gravatar($recent_comment->email, 47) }}" alt="{{ $recent_comment->name }}">
                     </div>
                     <div class="content">
-                        <p>demo says:</p>
-                        <a href="#">Quisque semper nunc vitae...</a>
+                        <p>{{ $recent_comment->name }} says:</p>
+                        <a href="#">{{ Str::limit($recent_comment->comment, 22, '...') }}</a>
                     </div>
                 </div>
             </li>
-            <li>
-                <div class="post-wrapper">
-                    <div class="thumb">
-                        <img src="{{ asset('frontend/images/blog/comment/1.jpeg') }}" alt="Comment images">
-                    </div>
-                    <div class="content">
-                        <p>Admin says:</p>
-                        <a href="#">Curabitur aliquet pulvinar...</a>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <div class="post-wrapper">
-                    <div class="thumb">
-                        <img src="{{ asset('frontend/images/blog/comment/1.jpeg') }}" alt="Comment images">
-                    </div>
-                    <div class="content">
-                        <p>Irin says:</p>
-                        <a href="#">Quisque semper nunc vitae...</a>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <div class="post-wrapper">
-                    <div class="thumb">
-                        <img src="{{ asset('frontend/images/blog/comment/1.jpeg') }}" alt="Comment images">
-                    </div>
-                    <div class="content">
-                        <p>Boighor says:</p>
-                        <a href="#">Quisque semper nunc vitae...</a>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <div class="post-wrapper">
-                    <div class="thumb">
-                        <img src="{{ asset('frontend/images/blog/comment/1.jpeg') }}" alt="Comment images">
-                    </div>
-                    <div class="content">
-                        <p>demo says:</p>
-                        <a href="#">Quisque semper nunc vitae...</a>
-                    </div>
-                </div>
-            </li>
+            @endforeach
         </ul>
     </aside>
     <!-- End Single Widget -->
@@ -104,13 +62,9 @@
     <aside class="widget category_widget">
         <h3 class="widget-title">Categories</h3>
         <ul>
-            <li><a href="#">Fashion</a></li>
-            <li><a href="#">Creative</a></li>
-            <li><a href="#">Electronics</a></li>
-            <li><a href="#">Kids</a></li>
-            <li><a href="#">Flower</a></li>
-            <li><a href="#">Books</a></li>
-            <li><a href="#">Jewelle</a></li>
+            @foreach($global_categories as $global_category)
+                <li><a href="#">{{ $global_category->name }}</a></li>
+            @endforeach
         </ul>
     </aside>
     <!-- End Single Widget -->
@@ -118,11 +72,9 @@
     <aside class="widget archives_widget">
         <h3 class="widget-title">Archives</h3>
         <ul>
-            <li><a href="#">March 2015</a></li>
-            <li><a href="#">December 2014</a></li>
-            <li><a href="#">November 2014</a></li>
-            <li><a href="#">September 2014</a></li>
-            <li><a href="#">August 2014</a></li>
+            @foreach($global_archives as $year => $month)
+                <li><a href="#">{{ date("F", mktime(0, 0, 0, $year, 1)) . ' ' . $month }}</a></li>
+            @endforeach
         </ul>
     </aside>
     <!-- End Single Widget -->
