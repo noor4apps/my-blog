9cc8af0b3bd37ae6de99d714b0b8b02927b13fe3
noor4apps <noor4apps@gmail.com>
2021-05-30 19:59:05 +0300

    #45

--- app/Http/Controllers/Frontend/UsersController.php
+++ app/Http/Controllers/Frontend/UsersController.php
@@ -10,7 +10,9 @@ use App\Models\PostMedia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 use Stevebauman\Purify\Facades\Purify;
 
@@ -306,12 +308,86 @@ class UsersController extends Controller
 
     public function update_info(Request $request)
     {
-        //
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'mobile' => 'required|numeric',
+            'bio' => 'nullable|min:10',
+            'receive_email' => 'required',
+            'user_image' => 'nullable|image|max:20000,mimes:jpeg,jpg,png',
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->withErrors($validation)->withInput();
+        }
+        $data['name' ] = $request->name;
+        $data['mobile'] = $request->email;
+        $data['mobile'] = $request->mobile;
+        $data['bio'] = $request->bio;
+        $data['receive_email'] = $request->receive_email;
+
+        if($image = $request->file('user_image')) {
+            if(auth()->user()->user_image != '') {
+                if(File::exists('assets/users/' . auth()->user()->user_image)){
+                    unlink('assets/users/' . auth()->user()->user_image);
+                }
+            }
+
+            $filename = Str::slug(auth()->user()->username) . '.' . $image->getClientOriginalExtension();
+            $path = public_path('assets/users/' . $filename);
+            Image::make($image->getRealPath())->resize(300, 300, function ($constraint){
+                $constraint->aspectRatio();
+            })->save($path, 100);
+
+            $data['user_image' ] = $filename;
+        }
+
+        $update = auth()->user()->update($data);
+        if($update) {
+            return redirect()->back()->with([
+                'message' => 'Information updated successfully.',
+                'alert-type' => 'success',
+            ]);
+        }
+        return redirect()->back()->with([
+            'message' => 'Something was wrong.',
+            'alert-type' => 'danger',
+        ]);
+
     }
 
     public function update_password(Request $request)
     {
-        //
+        $validation = Validator::make($request->all(), [
+            'current_password' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->withErrors($validation)->withInput();
+        }
+
+        $user = auth()->user();
+        if(Hash::check($request->current_password, $user->password)){
+            $update = $user->update([
+                'password' => bcrypt($request->password)
+            ]);
+
+            if($update) {
+                return redirect()->back()->with([
+                    'message' => 'Password updated successfully.',
+                    'alert-type' => 'success',
+                ]);
+            } else {
+                return redirect()->back()->with([
+                    'message' => 'Something was wrong.',
+                    'alert-type' => 'danger',
+                ]);
+            }
+        } else {
+            return redirect()->back()->with([
+                'message' => 'Something was wrong.',
+                'alert-type' => 'danger',
+            ]);
+        }
     }
 
 }
