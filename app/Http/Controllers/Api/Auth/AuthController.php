9a4b6b0a97b82ad3b4be7a089ca4f93266a631cc
noor4apps <noor4apps@gmail.com>
2021-09-18 15:50:01 +0300

    15 register and login and refreshedToken

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'mobile' => ['required', 'numeric', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+        if ($validation->fails()) {
+            return response()->json(['errors' => true, 'messages' => $validation->errors()], 200);
+        }
+
+        $data['name'] = $request->name;
+        $data['username'] = $request->username;
+        $data['email'] = $request->email;
+        $data['email_verified_at'] = Carbon::now();
+        $data['mobile'] = $request->mobile;
+        $data['password'] = bcrypt($request->password);
+        $data['status'] = 1;
+
+        $user = User::create($data);
+        $user->attachRole(Role::whereName('user')->first()->id);
+
+        return $this->getRefreshedToken($request->email, $request->password);
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['username' => $request->username, 'password' => $request->password])) {
+            $user = Auth::user();
+            $email = $user->email;
+            return $this->getRefreshedToken($email, $request->password);
+        } else {
+            return response()->json(['errors' => true, 'message' => 'Unauthorized']);
+        }
+    }
+
+    public function getRefreshedToken($email, $password)
+    {
+
+        $verifyValue = app()->environment() == 'local' ? false : true;
+
+        $response = Http::withOptions([
+            'verify' => $verifyValue,
+        ])->post(config('app.url') . '/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => config('passport.personal_access_client.id'),
+            'client_secret' => config('passport.personal_access_client.secret'),
+            'username' => $email,
+            'password' => $password,
+            'scope' => '*',
+        ]);
+
+        return response()->json($response->json(), 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Api\ApiController;
 use App\Http\Controllers\Api\General\GeneralController;
+use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::get('/chart/comments_chart',     [ApiController::class, 'comments_chart']);
 Route::get('/chart/users_chart',        [ApiController::class, 'users_chart']);
@@ -14,8 +15,8 @@ Route::get('/chart/users_chart',        [ApiController::class, 'users_chart']);
 Route::get('get-posts',                 [GeneralController::class, 'get_posts']);
 Route::get('post/{slug}',               [GeneralController::class, 'show_post']);
 
-Route::post('register',                 [\App\Http\Controllers\Frontend\Auth\RegisterController::class, 'register']);
-Route::post('login',                    [\App\Http\Controllers\Frontend\Auth\LoginController::class, 'login']);
+Route::post('register',                 [AuthController::class, 'register']);
+Route::post('login',                    [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->get('/user', function (\Illuminate\Http\Request $request) {
     return $request->user();
